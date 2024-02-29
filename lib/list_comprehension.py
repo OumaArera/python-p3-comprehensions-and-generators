0f7bfc9ca22870d6507b1e5ac8e9c761be214bf8
OumaArera <johnouma999@gmail.com>
2024-02-29 14:01:54 +0300

    feat: complete assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_lis = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_lis.append(num)
+    return even_lis
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
