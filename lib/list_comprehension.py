9f71ddded91d9b8ddf0a9e6b52372dec880a89e0
OumaArera <johnouma999@gmail.com>
2024-02-29 14:03:49 +0300

    optimize

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    even_lis = []
-    for num in num_list:
-        if num % 2 == 0:
-            even_lis.append(num)
-    return even_lis
+    return [ num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
     return [sentence + "!" for sentence in sentence_list]
